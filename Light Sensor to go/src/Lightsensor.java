9798992c8a6972fa766b8842fdaef457c1807240
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-27 14:54:21 +0100

    speed

--- Light Sensor to go/src/Lightsensor.java
+++ Light Sensor to go/src/Lightsensor.java
@@ -4,12 +4,14 @@ import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 
 public class Lightsensor {
+	private static final float BREAK_COOEFICIENT = 0; //10f;
 	private static final int MIDDLE_LIGHT_VALUE = 45;
 	private static final int BASE_SPEED = 500;
-	private static final float Kp = 3;
-	private static final float Ki = 2;
+	private static final float Kp = 4;
+	private static final float Ki = 0; //2;
 	private static final float Kd = 0f;
-	private static final float DELTA_T = 0.5f;
+	private static final long DELTA_T_ms = 10; // in milliseconds
+	private static final float DELTA_T = DELTA_T_ms / 1000f; // in seconds
 
 	public static void main(String[] args) throws Exception {
 		LightSensor light = new LightSensor(SensorPort.S1);
@@ -19,23 +21,36 @@ public class Lightsensor {
 		Motor.B.setSpeed(BASE_SPEED);
 		float errorIntegrated = 0;
 		float errorDerivated = 0;
-		float lastError = 0;
+		int lastError = 0;
 		float oldPowerMotorA = 0;
 		float oldPowerMotorB = 0;
+		float maxBreak = 0;
+		int i = 0;
 		while (true) {
+			if(i % 10 == 0) {
+				System.out.println(maxBreak);
+				maxBreak = 0;
+			}
 			int lightValue = light.getLightValue();
 			int error = lightValue - MIDDLE_LIGHT_VALUE;
+			if((error - lastError) > 1) {
+				//System.out.println(error - lastError);
+			}
 			errorIntegrated = 2f / 3f * errorIntegrated + DELTA_T * error;
 			errorDerivated = (error - lastError) / DELTA_T;
 			float compensation = error * Kp + errorIntegrated * Ki + errorDerivated * Kd;
-			System.out.println(errorDerivated);
-			float powerMotorA = BASE_SPEED + compensation;
-			float powerMotorB = BASE_SPEED - compensation;
+			float motorBreak = Math.abs(errorDerivated) * BREAK_COOEFICIENT;
+			maxBreak = Math.max(maxBreak, motorBreak);
+			float powerMotorA = BASE_SPEED - motorBreak + compensation;
+			float powerMotorB = BASE_SPEED - motorBreak - compensation;
 			setMotorPower(Motor.A, powerMotorA, oldPowerMotorA);
 			setMotorPower(Motor.B, powerMotorB, oldPowerMotorB);
 			lastError = error;
 			oldPowerMotorA = powerMotorA;
 			oldPowerMotorB = powerMotorB;
+			i++;
+			
+			Thread.sleep(DELTA_T_ms);
 		}
 	}
 
