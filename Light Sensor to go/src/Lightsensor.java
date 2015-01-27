71d9d2fdb1169c29b8994285c2c42936f5168978
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-27 13:19:02 +0100

    P-Controller

--- Light Sensor to go/src/Lightsensor.java
+++ Light Sensor to go/src/Lightsensor.java
@@ -2,27 +2,24 @@ import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.Motor;
 
-
-
 public class Lightsensor {
-	 public static void main(String[] args)  throws Exception {
-             LightSensor light= new LightSensor(SensorPort.S1);
-            	     while(true){
-            	       int a;
-            	       a=light.getLightValue();
-            	       if (a>=40 && a< 50){
-            	    	   Motor.A.forward();
-            	    	   Motor.B.forward();
-            	    	   }
-            	       else if (a<40){
-            	    	   Motor.A.forward();
-            	    	   Motor.B.stop();
-            	       }   
-            	       
-            	       else {
-            	    	   Motor.A.stop();
-            	    	   Motor.B.forward();
-            	       }   
-     }
- }
+	private static final int MIDDLE_LIGHT_VALUE = 45;
+	private static final int BASE_SPEED = 300;
+	private static final float Kp = 5;
+
+	public static void main(String[] args) throws Exception {
+		LightSensor light = new LightSensor(SensorPort.S1);
+		Motor.A.forward();
+		Motor.B.forward();
+		Motor.A.setSpeed(BASE_SPEED);
+		Motor.B.setSpeed(BASE_SPEED);
+		while (true) {
+			int lightValue = light.getLightValue();
+			int error = lightValue - MIDDLE_LIGHT_VALUE;
+			float compensation = error * Kp;
+			System.out.println(compensation);
+			Motor.A.setSpeed(BASE_SPEED + compensation);
+			Motor.B.setSpeed(BASE_SPEED - compensation);
+		}
+	}
 }
