6eefcf323e12cc781886c0beab189e3fcf8c6312
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-27 14:06:40 +0100

    Handle negative velocity

--- Light Sensor to go/src/Lightsensor.java
+++ Light Sensor to go/src/Lightsensor.java
@@ -1,13 +1,15 @@
 import lejos.nxt.LightSensor;
+import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.Motor;
+import lejos.robotics.RegulatedMotor;
 
 public class Lightsensor {
 	private static final int MIDDLE_LIGHT_VALUE = 45;
 	private static final int BASE_SPEED = 500;
 	private static final float Kp = 3;
 	private static final float Ki = 2;
-	private static final float Kd = 2f;
+	private static final float Kd = 4f;
 	private static final float DELTA_T = 0.5f;
 
 	public static void main(String[] args) throws Exception {
@@ -26,9 +28,19 @@ public class Lightsensor {
 			errorDerivated = (error - lastError) / DELTA_T;
 			float compensation = error * Kp + errorIntegrated * Ki + errorDerivated * Kd;
 			System.out.println(errorDerivated);
-			Motor.A.setSpeed(BASE_SPEED + compensation);
-			Motor.B.setSpeed(BASE_SPEED - compensation);
+			setMotorPower(Motor.A, BASE_SPEED + compensation);
+			setMotorPower(Motor.B, BASE_SPEED - compensation);
 			lastError = error;
+		}
+	}
+
+	private static void setMotorPower(NXTRegulatedMotor motor, float power) {
+		float absPower = Math.abs(power);
+		motor.setSpeed(absPower);
+		if(power > 0) {
+			motor.forward();
+		} else {
+			motor.backward();
 		}
 	}
 }
