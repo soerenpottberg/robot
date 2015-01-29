5b81e22826bf0588ffe6edc90e6e87ad42499d23
Sören Pottberg <soerenpottberg@web.de>
2015-01-29 10:00:55 +0100

    PI controller not perfect.

--- Light Sensor to go/src/Lightsensor.java
+++ Light Sensor to go/src/Lightsensor.java
@@ -8,16 +8,16 @@ public class Lightsensor {
 	private static final int MIDDLE_LIGHT_VALUE = 40;
 	private static final int BASE_POWER = 30;
 
-	private static final double K_CRITICAL = 1.5;
+	private static final double K_CRITICAL = 3;
 	private static final double T_PERIOD = 0.05;
 	private static final double DELTA_2 = 2.5 / 1000; // 2-3 ms
+	
+	private static final double Kp_CALC = 0.45 * K_CRITICAL;  // 0.6
+	private static final double Ki_CALC = 1.2 * Kp_CALC * DELTA_2 / T_PERIOD;  // 2
+	private static final double Kd_CALC = Kp_CALC * T_PERIOD / (8 * DELTA_2);  // 1
 
-	private static final double Kp_CALC = 0.6 * K_CRITICAL;
-	private static final double Ki_CALC = 2 * Kp_CALC * DELTA_2 / T_PERIOD;
-	private static final double Kd_CALC = Kp_CALC * T_PERIOD / (8 * DELTA_2);
-
-	private static final int Kp = (int) ((0 * Kp_CALC + 3) * 100);
-	private static final int Ki = (int) (0 * Ki_CALC * 100) + 2;
+	private static final int Kp = (int) ((0 * Kp_CALC + 3.5) * 100); // 1.0
+	private static final int Ki = (int) ((0 * Ki_CALC + 0.05) * 100) + 2; // 0.25
 	private static final int Kd = (int) (0 * Kd_CALC * 100);
 
 	public static void main(String[] args) throws Exception {
@@ -42,7 +42,7 @@ public class Lightsensor {
 			System.out.println(errorIntegrated);
 			if (error > 0) {
 				//Sound.beep();
-				errorIntegrated += error;
+				//errorIntegrated += 2* error;
 			}
 			errorDerivated = (error - lastError);
 			int compensation = (error * Kp + errorIntegrated * Ki + errorDerivated
