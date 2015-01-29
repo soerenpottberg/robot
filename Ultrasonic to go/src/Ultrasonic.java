f1f8782b75880442a1ffbb44dee49f4e9a9f191c
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-29 16:11:31 +0100

    basic Ultrasonic

--- Ultrasonic to go/src/Ultrasonic.java
+++ Ultrasonic to go/src/Ultrasonic.java
@@ -10,62 +10,23 @@ public class Ultrasonic {
 			int a = Ultra.getDistance();
 			pilot.setTravelSpeed(20);
 			pilot.forward();
-			int error = 20-a;
 			
-			if (error > 5){
-				pilot.travelArc(10, 10);
-				pilot.forward();
-			}
-			else if (error < -5){
-				pilot.travelArc(-10, 10);
-				pilot.travelArc(10, 10);
-				pilot.forward();
+			if (a<10) {
+				pilot.rotate(10);
+				pilot.travel(10);
+				pilot.rotate(-10);
 			}
 			
-			/*int errordistance = 20-a;
-			if (errordistance > 10) {
-				pilot.travelArc(-5, 5);
-				pilot.travelArc(5,5);
-				pilot.forward();
+			else if (a > 20 && a < 50){
+				pilot.rotate(-10);
+				pilot.travel(15);
+				pilot.rotate(10);
 			}
-			else if (errordistance < -10) {
-				pilot.steer(10);
-			} 
-			
-			//int error = 2 * errordistance;
-			
-			/*else if (Math.abs(errordistance)<5){
-				pilot.forward();
-				//pilot.arcForward(Math.signum(errordistance)*15+ 2*error);
+			else if(a > 55){
+				pilot.travel(5);
+				pilot.rotate(-90);
+				pilot.travel(25);
 			}
-			else {
-				pilot.steer(-errordistance);
-				//pilot.arcForward(Math.signum(errordistance)*15+error);
-			}
-			// int b = (a + 5) % 255;
-			/*Motor.A.forward();
-			Motor.B.forward();
-			int errordistance = 20 - a;
-			if (errordistance > 10) {
-				errordistance = 10;
-			}
-			if (errordistance < -10) {
-				errordistance = -10;
-			}
-			int error = 2 * errordistance;
-
-			Motor.A.setSpeed(Startspeed + error);
-			Motor.B.setSpeed(Startspeed - error);
-			System.out.println(error);
-
-			
-			 * int oldb=35; if (b < 20) { Motor.A.setSpeed(Startspeed + error);
-			 * Motor.B.setSpeed(Startspeed - error); } else if (b >= 20 && b <=
-			 * 25) { Motor.A.setSpeed(Startspeed); Motor.B.setSpeed(Startspeed);
-			 * } else if (b>25&& b<=35){ Motor.A.setSpeed(Startspeed - error);
-			 * Motor.B.setSpeed(Startspeed + error); else{ if(Math.abs(b-old)<2)
-			 * } }
-			 */
 		}
 	}
 }
