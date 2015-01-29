43d1507496c9a400513d6b6101b34a783d1b5c5b
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-29 14:30:17 +0100

    temp commit

--- Distance Sensor/src/distance.java
+++ Distance Sensor/src/distance.java
@@ -3,7 +3,7 @@ import lejos.nxt.*;
 
 public class distance {
 	public static void main(String[] args) throws Exception {
-		UltrasonicSensor ultrasonic = new UltrasonicSensor(SensorPort.S2);
+		UltrasonicSensor ultrasonic = new UltrasonicSensor(SensorPort.S3);
 		while (true) {
 			int b;
 			b = ultrasonic.getDistance();
--- Ultrasonic to go/src/Ultrasonic.java
+++ Ultrasonic to go/src/Ultrasonic.java
@@ -1,26 +1,71 @@
 import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Ultrasonic {
-	public static void main(String[] args) throws Exception{
-		UltrasonicSensor ultrasonic = new UltrasonicSensor(SensorPort.S2);
+	public static void main(String[] args) throws Exception {
+		UltrasonicSensor Ultra = new UltrasonicSensor(SensorPort.S3);
+		DifferentialPilot pilot= new DifferentialPilot(8.16, 13, Motor.B,Motor.A);
+
 		while (true) {
-			int b;
-			b = ultrasonic.getDistance();
-			System.out.println(b);
-			if (b<12){
-				Motor.B.forward();
-				Motor.A.stop();
+			int a = Ultra.getDistance();
+			pilot.setTravelSpeed(20);
+			pilot.forward();
+			int error = 20-a;
+			
+			if (error > 5){
+				pilot.travelArc(10, 10);
+				pilot.forward();
 			}
-			else if(b>=12&&b<=15) {
-				Motor.B.forward();
-				Motor.A.forward();
+			else if (error < -5){
+				pilot.travelArc(-10, 10);
+				pilot.travelArc(10, 10);
+				pilot.forward();
 			}
-			else{
-				Motor.B.stop();
-				Motor.A.forward();
+			
+			/*int errordistance = 20-a;
+			if (errordistance > 10) {
+				pilot.travelArc(-5, 5);
+				pilot.travelArc(5,5);
+				pilot.forward();
 			}
+			else if (errordistance < -10) {
+				pilot.steer(10);
+			} 
+			
+			//int error = 2 * errordistance;
+			
+			/*else if (Math.abs(errordistance)<5){
+				pilot.forward();
+				//pilot.arcForward(Math.signum(errordistance)*15+ 2*error);
+			}
+			else {
+				pilot.steer(-errordistance);
+				//pilot.arcForward(Math.signum(errordistance)*15+error);
+			}
+			// int b = (a + 5) % 255;
+			/*Motor.A.forward();
+			Motor.B.forward();
+			int errordistance = 20 - a;
+			if (errordistance > 10) {
+				errordistance = 10;
+			}
+			if (errordistance < -10) {
+				errordistance = -10;
+			}
+			int error = 2 * errordistance;
+
+			Motor.A.setSpeed(Startspeed + error);
+			Motor.B.setSpeed(Startspeed - error);
+			System.out.println(error);
+
+			
+			 * int oldb=35; if (b < 20) { Motor.A.setSpeed(Startspeed + error);
+			 * Motor.B.setSpeed(Startspeed - error); } else if (b >= 20 && b <=
+			 * 25) { Motor.A.setSpeed(Startspeed); Motor.B.setSpeed(Startspeed);
+			 * } else if (b>25&& b<=35){ Motor.A.setSpeed(Startspeed - error);
+			 * Motor.B.setSpeed(Startspeed + error); else{ if(Math.abs(b-old)<2)
+			 * } }
+			 */
 		}
-
 	}
-
 }
