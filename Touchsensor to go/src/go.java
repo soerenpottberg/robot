bdea045a19919281a569a8c4d9de1090a5c2c5af
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-01-29 11:54:07 +0100

    Touchsensor

---
+++ Touchsensor to go/src/go.java
@@ -0,0 +1,25 @@
+import lejos.nxt.*;
+import lejos.robotics.Touch;
+import lejos.robotics.navigation.DifferentialPilot;
+
+public class go {
+	public static void main(String[] args) throws Exception {
+		DifferentialPilot pilot= new DifferentialPilot(8.16, 13, Motor.B,Motor.A);
+
+		TouchSensor touchl=new TouchSensor(SensorPort.S2);
+		TouchSensor touchr= new TouchSensor(SensorPort.S4);
+		while(true){
+			pilot.setTravelSpeed(20);
+			pilot.forward();
+			if (touchr.isPressed()){
+				pilot.travel(-5);
+				pilot.travelArc(8,5);
+			} 
+			else if (touchl.isPressed()){
+				pilot.travel(-5);
+				pilot.travelArc(-8,5);
+			} 
+		}
+		
+	}
+}
