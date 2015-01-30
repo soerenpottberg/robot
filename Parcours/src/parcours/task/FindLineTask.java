5a936dff1aebabb772bda5260e07079293cd4b37
Nico Weyand <nico.weyand@student.kit.edu>
2015-01-30 21:48:37 +0100

    Added a foolproof and straightforward touch sensor impact handling mechanism to the FindLineTask. TODO: Extract variables to constants.

--- Parcours/src/parcours/task/FindLineTask.java
+++ Parcours/src/parcours/task/FindLineTask.java
@@ -1,6 +1,8 @@
 package parcours.task;
 
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 import parcours.detector.LineDetector;
 
@@ -8,18 +10,35 @@ public class FindLineTask extends Task {
 
 	private DifferentialPilot pilot;
 	private LineDetector lineDetector;
+	private TouchSensor touchR, touchL;
 
 	@Override
 	protected void init() {
+		touchR = new TouchSensor(SensorPort.S4);
+		touchL = new TouchSensor(SensorPort.S2);
+		
 		pilot = new DifferentialPilot(8.16, 13, Motor.B, Motor.A);
 		lineDetector = new LineDetector();
 		pilot.setTravelSpeed(20);
+		pilot.travel(15);
 		pilot.forward();
 	}
 
 	@Override
 	protected void control() {
-		// TODO check for collision		
+		if ( touchR.isPressed() ) {
+			pilot.stop();
+			pilot.travel(-3);
+			pilot.rotate(+10);
+			pilot.forward();
+		}
+		
+		if (touchL.isPressed() ) {
+			pilot.stop();
+			pilot.travel(-3);
+			pilot.rotate(-10);
+			pilot.forward();
+		}
 	}
 
 	@Override
