8ab34c5dde26983cc6247dc250bbb95bb5425016
Sören Pottberg <soerenpottberg@web.de>
2015-01-30 08:00:04 +0100

    Add implementation of follow right task from project "Ultrasonic to go"

--- Parcours/src/parcours/task/FollowRightLineTask.java
+++ Parcours/src/parcours/task/FollowRightLineTask.java
@@ -1,17 +1,40 @@
 package parcours.task;
 
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 public class FollowRightLineTask extends Task {
+
+	private UltrasonicSensor ultra;
+	private DifferentialPilot pilot;
 
 	@Override
 	protected void init() {
-		// TODO Auto-generated method stub
-		
+		ultra = new UltrasonicSensor(SensorPort.S3);
+		pilot = new DifferentialPilot(8.16, 13, Motor.B, Motor.A);
 	}
 
 	@Override
 	protected void control() {
-		// TODO Auto-generated method stub
-		
+		int a = ultra.getDistance();
+		pilot.setTravelSpeed(20);
+		pilot.forward();
+
+		if (a < 10) {
+			pilot.rotate(10);
+			pilot.travel(10);
+			pilot.rotate(-10);
+		} else if (a > 20 && a < 50) {
+			pilot.rotate(-10);
+			pilot.travel(15);
+			pilot.rotate(10);
+		} else if (a > 55) {
+			pilot.travel(5);
+			pilot.rotate(-90);
+			pilot.travel(25);
+		}
 	}
 
 	@Override
@@ -19,7 +42,5 @@ public class FollowRightLineTask extends Task {
 		// TODO Return true if a line is detected
 		return false;
 	}
-
-	
 
 }
