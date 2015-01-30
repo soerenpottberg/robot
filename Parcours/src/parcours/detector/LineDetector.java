ecab1a7f275393cc3cdca390846446cb8a69cd0c
Nico Weyand <nico.weyand@student.kit.edu>
2015-01-30 22:15:57 +0100

    Implemented more robust LineDetector. Note that it will perform better, the shorter the given cycle time. Current cycle times are about 36-39 ms, which is barely short enough... Cycle times < 25 ms would be optimal...

--- Parcours/src/parcours/detector/LineDetector.java
+++ Parcours/src/parcours/detector/LineDetector.java
@@ -1,7 +1,8 @@
 package parcours.detector;
 
+import debug.DebugOutput;
+import debug.TimingDebug;
 import sensor.evaluation.LightSensorEvaluation;
-import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 
 /**
@@ -12,25 +13,45 @@ import lejos.nxt.SensorPort;
  *
  */
 public class LineDetector {
+	private static final int CYCLE_TIME_STARTING_VALUE = 1000;
+	private static final int WHITE_LINE_DETECTION_THRESHOLD = 39;
+	private static final float EWMA_ALPHA = 0.7f;
+	private static final int DETECTION_TIME_MS = 50;
 	
-	private static final int WHITE_LINE_THRESHOLD = 50;
-	private static final float EWMA_ALPHA = 0.1f;
-	private static final int SAMPLING_COUNT = 25;
-
+	private DebugOutput out;
+	private TimingDebug timingDebug;
+	
+	private long tLastCycleStart;
+	
 	private LightSensorEvaluation evaluation;
-	private int count = 0;
+	private float tCurrentDetectionIntervalLength = 0.0f;
 
 	public LineDetector() {
-		evaluation = new LightSensorEvaluation( SensorPort.S1, EWMA_ALPHA, WHITE_LINE_THRESHOLD );
+		// This makes sure that the detector generates no hit after its initialization if the first measure is by chance a hit.
+		tLastCycleStart = System.currentTimeMillis() + CYCLE_TIME_STARTING_VALUE;
+		
+		evaluation = new LightSensorEvaluation( SensorPort.S1, EWMA_ALPHA, WHITE_LINE_DETECTION_THRESHOLD );
+		
+		// for debugging purposes only:
+		out = new DebugOutput();
+		timingDebug = new TimingDebug(out, 10, 0, 1);
 	}
 
 	public boolean hasDetected() {
+		// get the current cycle length (smoothed)
+		final long tNow = System.currentTimeMillis();
+		final long tDifference = tNow - tLastCycleStart;
+		tLastCycleStart = tNow;
+		
+		// debug output:
+		timingDebug.triggerCycle();
+		
 		if ( evaluation.measureError() > 0 ) {
-			++count;
+			tCurrentDetectionIntervalLength += tDifference;
 		} else {
-			count = 0;
+			tCurrentDetectionIntervalLength = 0;
 		}
 		
-		return count > SAMPLING_COUNT;
+		return tCurrentDetectionIntervalLength > DETECTION_TIME_MS;
 	}
 }
