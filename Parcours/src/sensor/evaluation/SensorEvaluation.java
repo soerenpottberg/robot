c129c4cce99c25172d979966bd8e393287f2fe14
Nico Weyand <nico.weyand@student.kit.edu>
2015-01-30 21:42:41 +0100

    Moved ewma calculation to own utility method.

--- Parcours/src/sensor/evaluation/SensorEvaluation.java
+++ Parcours/src/sensor/evaluation/SensorEvaluation.java
@@ -1,14 +1,15 @@
 package sensor.evaluation;
 
+import utils.EWMA;
+
 /**
  * The Sensor class provides an interface for sensors to be used with
  * the PID control.
  * @author Nico Weyand
  */
 public abstract class SensorEvaluation {
-	private float alpha;
+	private EWMA ewma;
 	private int targetValue;
-	private float ewma;
 	
 	/**
 	 * Creates a new Sensor with support for value smooting based on
@@ -17,15 +18,8 @@ public abstract class SensorEvaluation {
 	 * @param targetValue
 	 */
 	public SensorEvaluation(float alpha, int targetValue) {
-		this.alpha = alpha;
+		this.ewma = new EWMA(alpha, targetValue);
 		this.targetValue = targetValue;
-	}
-	
-	public float getWeightingDecrease() {
-		return alpha;
-	}
-	public void setWeightingDecrease(float alpha) {
-		this.alpha = alpha;
 	}
 	
 	public int getTargetValue() {
@@ -42,15 +36,6 @@ public abstract class SensorEvaluation {
 	 * @return The sensor data reduced by an offset of expected_value.
 	 */
 	public float measureError() {
-		caclulateEWMA();
-		return calculateError();
-	}
-
-	private void caclulateEWMA() {
-		ewma = ( 1 - alpha ) * ewma + alpha * getSensorData();
-	}
-
-	private float calculateError() {
-		return ewma - targetValue;
+		return ewma.addValue(getSensorData()) - targetValue;
 	}
 }
---
+++ Parcours/src/utils/EWMA.java
@@ -0,0 +1,46 @@
+package utils;
+
+/**
+ * This class implements an exponential weightenend moving average (EWMA) algorithm.
+ * @author Nico Weyand
+ *
+ */
+public class EWMA {
+	private float alpha;
+	private float ewma;
+	
+	/**
+	 * @param alpha : The weightening value
+	 * @param initialValue : The initial value of the average. Setting this to a
+	 * value close to the center of the expected range of input values allows for
+	 * your ewma to converge faster with your measures.
+	 */
+	public EWMA(float alpha, float initialValue) {
+		this.alpha = alpha;
+		this.ewma  = initialValue;
+	}
+	
+	public float getWeightingDecrease() {
+		return alpha;
+	}
+	public void setWeightingDecrease(float alpha) {
+		this.alpha = alpha;
+	}
+	
+	/**
+	 * Adds a new value to the ewma. 
+	 * @param value : The value to be added.
+	 * @return The current value of the ewma.
+	 */
+	public float addValue(float value) {
+		ewma = ( 1 - alpha ) * ewma + alpha * value;
+		return ewma;
+	}
+	
+	/**
+	 * @return The current value of the ewma.
+	 */
+	public float value() {
+		return ewma;
+	}
+}
