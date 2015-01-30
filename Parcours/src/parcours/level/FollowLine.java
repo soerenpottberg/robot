21afc43484e5982f42c5c68c6ca9b8dab66d6c09
Sören Pottberg <soerenpottberg@web.de>
2015-01-30 08:25:16 +0100

    Add implementation of FollowLineTask from project "Lightsensor to go"

--- Parcours/src/parcours/level/FollowLine.java
+++ Parcours/src/parcours/level/FollowLine.java
@@ -1,10 +1,19 @@
 package parcours.level;
 
+import parcours.task.FollowLineTask;
 import parcours.task.Task;
 
 
 public class FollowLine extends Level {
 
+	private static int TASK_COUNT = 1;
+	private static Task[] tasks = new Task[TASK_COUNT];
+	
+	static {
+		int i = 0;
+		tasks[i++] = new FollowLineTask();
+	}
+	
 	@Override
 	public String getLabel() {
 		return "Follow Line";
