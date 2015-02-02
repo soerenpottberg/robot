0507313fd6e97cb46d354055e7987321d8605bac
Sören Pottberg <soerenpottberg@web.de>
2015-02-02 12:19:47 +0100

    Bridge edge detection

--- Parcours/src/parcours/level/Bridge.java
+++ Parcours/src/parcours/level/Bridge.java
@@ -1,16 +1,18 @@
 package parcours.level;
 
+import parcours.task.FindBridgeEdgeTask;
 import parcours.task.FollowBridgeTask;
 import parcours.task.Task;
 
 
 public class Bridge extends Level {
 	
-	private static int TASK_COUNT = 1;
+	private static int TASK_COUNT = 2;
 	private static Task[] tasks = new Task[TASK_COUNT];
 	
 	static {
 		int i = 0;
+		tasks[i++] = new FindBridgeEdgeTask();
 		tasks[i++] = new FollowBridgeTask();
 	}
 
