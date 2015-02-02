4466eb25dddb13cafc9e417052dd02f08fa3a4b5
Sören Pottberg <soerenpottberg@web.de>
2015-02-02 11:51:26 +0100

    Implement simple follow bridge algorithm

--- Parcours/src/parcours/level/Bridge.java
+++ Parcours/src/parcours/level/Bridge.java
@@ -1,9 +1,18 @@
 package parcours.level;
 
+import parcours.task.FollowBridgeTask;
 import parcours.task.Task;
 
 
 public class Bridge extends Level {
+	
+	private static int TASK_COUNT = 1;
+	private static Task[] tasks = new Task[TASK_COUNT];
+	
+	static {
+		int i = 0;
+		tasks[i++] = new FollowBridgeTask();
+	}
 
 	@Override
 	public String getLabel() {
@@ -12,8 +21,7 @@ public class Bridge extends Level {
 
 	@Override
 	public Task[] getTasks() {
-		// TODO Auto-generated method stub
-		return null;
+		return tasks;
 	}
 
 }
