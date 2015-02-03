3b603e9e619c0be13ef16c54e17c7eec27fb8502
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-02-03 12:34:59 +0100

    new motorrotate

--- Light Moving/src/MotorRotate.java
+++ Light Moving/src/MotorRotate.java
@@ -2,11 +2,10 @@ import lejos.nxt.*;
 
 public class MotorRotate {
 	public static void main(String[] args) throws Exception {
+		Motor.C.setSpeed(900);
 		while (true) {
-			Motor.C.rotate(90);
-			Thread.sleep(1000);
-			Motor.C.rotate(-90);
-			Thread.sleep(1000);
+			Motor.C.rotate(10);
+			Motor.C.rotate(-10);
 		}
 	}
 
