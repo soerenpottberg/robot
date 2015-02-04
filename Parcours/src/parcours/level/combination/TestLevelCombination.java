bc977764b63ff967e2eec9309aee79b1fa4168c4
zjwangzhenlei <zjwangzhenlei@gmail.com>
2015-02-04 19:09:11 +0100

    go into elevator, new version upcomming

--- Parcours/src/parcours/level/combination/TestLevelCombination.java
+++ Parcours/src/parcours/level/combination/TestLevelCombination.java
@@ -8,6 +8,7 @@ import parcours.level.base.LevelCombination;
 import parcours.level.test.DebugMeasureLevel;
 import parcours.level.test.TestElevatorBluetooth;
 import parcours.level.test.TestGateBluetooth;
+import parcours.level.test.TestGoIntoElevator;
 import parcours.level.test.TestRightTurnLevel;
 import parcours.level.test.TestTravelLevel;
 import parcours.level.test.TestTurnTableBluetooth;
@@ -28,6 +29,7 @@ public class TestLevelCombination extends LevelCombination {
 		levelList.add(new TestTurnTableBluetooth());
 		levelList.add(new TestGateBluetooth());
 		levelList.add(new TestElevatorBluetooth());
+		levelList.add(new TestGoIntoElevator());
 		return levelList;
 	}
 	
--- Parcours/src/parcours/task/GoIntoElevatorTask.java
+++ Parcours/src/parcours/task/GoIntoElevatorTask.java
@@ -3,34 +3,47 @@ package parcours.task;
 import lejos.nxt.TouchSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 import parcours.task.base.ControllerTask;
+import parcours.task.elevator.ElevatorContext;
+import parcours.task.labyrinth.LabyrinthContext;
 import parcours.utils.RobotDesign;
 
 public class GoIntoElevatorTask extends ControllerTask {
 	protected DifferentialPilot pilot;
 	protected TouchSensor touchSensorRight, touchSensorLeft;
+	private ElevatorContext context;
 	
 	@Override
 	protected void init() {
 		pilot = RobotDesign.differentialPilot;
 		touchSensorRight = RobotDesign.touchSensorRight;
 		touchSensorLeft = RobotDesign.touchSensorLeft;
+		context = new ElevatorContext(pilot);
 		pilot.setTravelSpeed(10);
+		pilot.forward();
 	}
 
 	@Override
 	protected void control() {
+		
+		boolean leftIsPressed = touchSensorLeft.isPressed();
+		boolean rightIsPressed = touchSensorRight.isPressed();
+
+		/*if (leftIsPressed && rightIsPressed) {
+		} else if (leftIsPressed) {
+			context.handleLeftButtonPressed();
+		} else if (rightIsPressed) {
+			context.handleRightButtonPressed();
+		} else {
+			context.handleNoButtonIsPressed();
+		}*/
+
 		pilot.forward();
-		if (touchSensorRight.isPressed()){
-			pilot.rotate(15);
-		}
-		if (touchSensorLeft.isPressed()){
-			pilot.rotate(-10);
-		}
+		
 	}
 
 	@Override
 	protected boolean abort() {
-		return (touchSensorRight.isPressed() && touchSensorLeft.isPressed());
+		return (touchSensorRight.isPressed() || touchSensorLeft.isPressed());
 	}
 
 	@Override
