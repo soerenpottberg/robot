916cf7aa61b0a9afee50566d2da94f778953ae10
Nico Weyand <nico.weyand@student.kit.edu>
2015-02-04 19:04:41 +0100

    Increased travel angle a little and added two explanatory comments.

--- Parcours/src/parcours/task/FindBridgeEdgeTask.java
+++ Parcours/src/parcours/task/FindBridgeEdgeTask.java
@@ -8,7 +8,7 @@ import parcours.utils.RobotDesign;
 
 
 public class FindBridgeEdgeTask extends ControllerTask {
-	private static final int SMALL_TURN_LEFT_ANGLE = 7;
+	private static final int SMALL_TURN_LEFT_ANGLE = 9;
 	private static final int SENSOR_EDGE_DETECTION_POSITION_ANGLE = 90;
 	private static final int BASE_SPEED = 20;
 	
@@ -20,7 +20,11 @@ public class FindBridgeEdgeTask extends ControllerTask {
 		pilot = RobotDesign.differentialPilot;
 		bridgeEdgeDetector = new BridgeEdgeDetector();
 		pilot.setTravelSpeed(BASE_SPEED);
+		
+		// move sensor to bridge position.
 		Motor.C.rotate(SENSOR_EDGE_DETECTION_POSITION_ANGLE);
+		
+		// make sure we reach the edge of the bridge during the incline and with a small angle
 		pilot.rotate(SMALL_TURN_LEFT_ANGLE);
 		pilot.forward();
 	}
