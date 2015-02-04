7cce64f1801707181134653bd56e38ca0515d81e
Sören Pottberg <soerenpottberg@web.de>
2015-02-04 10:55:45 +0100

    Integrate bluetooth communication into Obstacles level

--- Parcours/src/parcours/level/Obstacles.java
+++ Parcours/src/parcours/level/Obstacles.java
@@ -3,6 +3,7 @@ package parcours.level;
 import java.util.ArrayList;
 import java.util.List;
 
+import parcours.bluetooth.GateConnection;
 import parcours.level.base.Level;
 import parcours.task.FindLineTask;
 import parcours.task.FollowBridgeTask;
@@ -10,8 +11,9 @@ import parcours.task.FollowLineStraightTask;
 import parcours.task.FollowLineTaskRaw;
 import parcours.task.TraversOpenedDoorTask;
 import parcours.task.base.Task;
-import parcours.task.bluetooth.BluetoothCloseDoorTask;
-import parcours.task.bluetooth.BluetoothOpenDoorTask;
+import parcours.task.bluetooth.BluetoothCloseConnectionTask;
+import parcours.task.bluetooth.BluetoothCloseGateTask;
+import parcours.task.bluetooth.BluetoothConnectTask;
 
 
 public class Obstacles extends Level {
@@ -23,10 +25,12 @@ public class Obstacles extends Level {
 
 	@Override
 	public List<Task> createTaskList() {
+		final GateConnection connection = new GateConnection();
 		final List<Task> taskList = new ArrayList<Task>();
-		taskList.add(new BluetoothOpenDoorTask());
+		taskList.add(new BluetoothConnectTask(connection));
 		taskList.add(new TraversOpenedDoorTask());
-		taskList.add(new BluetoothCloseDoorTask());
+		taskList.add(new BluetoothCloseGateTask(connection));
+		taskList.add(new BluetoothCloseConnectionTask(connection));
 		taskList.add(new FindLineTask(false));
 		taskList.add(new FollowLineStraightTask());
 		taskList.add(new FollowBridgeTask());
