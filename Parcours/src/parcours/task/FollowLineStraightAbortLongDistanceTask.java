0a6e7896bd4a4707c57eba05dacfccef2b1d6027
Nico Weyand <nico.weyand@student.kit.edu>
2015-02-05 21:00:38 +0100

    ...

--- Parcours/src/parcours/task/FollowLineStraightAbortLongDistanceTask.java
+++ Parcours/src/parcours/task/FollowLineStraightAbortLongDistanceTask.java
@@ -99,7 +99,7 @@ public class FollowLineStraightAbortLongDistanceTask extends ControllerTask {
 		final float error = calculateError(lightValue);
 		
 		if ( Math.abs(lastError) > Math.abs(error) ) {
-			errorIntegrated *= 0.7f;
+			errorIntegrated *= 0.0f;
 		}
 		
 		integrateError(error);
