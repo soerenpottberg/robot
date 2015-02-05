2f322d84baa571abb0d3b345b5f50e037c8c0817
Nico Weyand <nico.weyand@student.kit.edu>
2015-02-05 19:38:50 +0100

    ...

--- Parcours/src/parcours/task/FollowLineStraightAbortLongDistanceTask.java
+++ Parcours/src/parcours/task/FollowLineStraightAbortLongDistanceTask.java
@@ -21,8 +21,8 @@ public class FollowLineStraightAbortLongDistanceTask extends ControllerTask {
 
 	private static final int BASE_POWER = 40;
 
-	private static final float Kp = 0.065f;
-	private static final float Ki = 0.0055f;
+	private static final float Kp = 0.070f;
+	private static final float Ki = 0.0045f;
 	private static final float Kd = 0.100f;
 
 	private LightSensor light;
