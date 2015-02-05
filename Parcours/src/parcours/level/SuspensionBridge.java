5fc6d946ab441d3477f171fa39a1bded1d39f4f9
Nico Weyand <nico.weyand@student.kit.edu>
2015-02-05 11:33:24 +0100

    Split up Obstacles Level into 3 smaller Levels.

---
+++ Parcours/src/parcours/level/SuspensionBridge.java
@@ -0,0 +1,32 @@
+package parcours.level;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import parcours.level.base.Level;
+import parcours.task.FindBridgeEdgeTask;
+import parcours.task.FindLineTask;
+import parcours.task.FollowBridgeSuspensionTask;
+import parcours.task.FollowLineStraightTask;
+import parcours.task.base.Task;
+
+
+public class SuspensionBridge extends Level {
+	
+	@Override
+	public String getLabel() {
+		return "SuspensionBridge";
+	}
+
+	@Override
+	public List<Task> createTaskList() {
+		final List<Task> taskList = new ArrayList<Task>();
+		taskList.add(new FindLineTask(false));
+		taskList.add(new FollowLineStraightTask());
+		taskList.add(new FindBridgeEdgeTask());
+		taskList.add(new FollowBridgeSuspensionTask());
+		taskList.add(new FindLineTask());
+		return taskList;
+	}
+
+}
--- Parcours/src/parcours/level/TurnTable.java
+++ Parcours/src/parcours/level/TurnTable.java
@@ -3,35 +3,29 @@ package parcours.level;
 import java.util.ArrayList;
 import java.util.List;
 
-import parcours.bluetooth.TurnTableConnection;
+import parcours.bluetooth.GateConnection;
 import parcours.level.base.Level;
+import parcours.task.TraversOpenedDoorTask;
 import parcours.task.base.Task;
 import parcours.task.bluetooth.BluetoothCloseConnectionTask;
+import parcours.task.bluetooth.BluetoothCloseGateTask;
 import parcours.task.bluetooth.BluetoothConnectTask;
-import parcours.task.bluetooth.BluetoothFinishTurnTableTask;
-import parcours.task.bluetooth.BluetoothTurnTurnTableTask;
-import parcours.task.bluetooth.BluetoothWaitForTurnTableTask;
 
 
-public class Swamp extends Level {
+public class TurnTable extends Level {
 	
 	@Override
 	public String getLabel() {
-		return "Swamp";
+		return "Obstacles";
 	}
 
 	@Override
 	public List<Task> createTaskList() {
-		final TurnTableConnection connection = new TurnTableConnection();
+		final GateConnection connection = new GateConnection();
 		final List<Task> taskList = new ArrayList<Task>();
 		taskList.add(new BluetoothConnectTask(connection));
-		taskList.add(new BluetoothWaitForTurnTableTask(connection));
-		
-		
-		taskList.add(new BluetoothTurnTurnTableTask(connection));
-		
-		
-		taskList.add(new BluetoothFinishTurnTableTask(connection));
+		taskList.add(new TraversOpenedDoorTask());
+		taskList.add(new BluetoothCloseGateTask(connection));
 		taskList.add(new BluetoothCloseConnectionTask(connection));
 		return taskList;
 	}
